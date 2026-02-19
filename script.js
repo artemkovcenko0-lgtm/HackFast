d7134be7bb1d2933540d98ec66cbce476018895e
artemkovcenko0-lgtm <artemkovcenko0@gmail.com>
2026-02-19 23:45:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 function openTab(evt, tabName) {
+    // Скрываем весь контент вкладок
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
         tabcontent[i].classList.remove("active");
     }
+
+    // Убираем класс active у всех кнопок
     tablinks = document.getElementsByClassName("tab-link");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].classList.remove("active");
     }
-    document.getElementById(tabName).style.display = "block";
-    document.getElementById(tabName).classList.add("active");
-    evt.currentTarget.className += " active";
+
+    // Показываем текущую вкладку и добавляем класс active кнопке
+    const currentTab = document.getElementById(tabName);
+    currentTab.style.display = "block";
+    setTimeout(() => currentTab.classList.add("active"), 10);
+    evt.currentTarget.classList.add("active");
 }
